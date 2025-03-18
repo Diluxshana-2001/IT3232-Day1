c5e17c22bd52ed327d2e3d921f4673237867e196
Diluxshana-2001 <diluxshana6@gmail.com>
2025-03-18 23:56:03 +0530

    created subclasses for fruits, vegetable and grocery

--- Task8.java
+++ Task8.java
@@ -31,3 +31,42 @@ abstract class Product {
 
     abstract void displayInfo();
 }
+
+class Fruit extends Product {
+    public Fruit(String name, double price, int quantity) {
+        super(name, price, quantity);
+    }
+
+    public void displayInfo() {
+        System.out.println("Fruit: " + getName());
+        System.out.println("Price: " + getPrice());
+        System.out.println("Quantity: " + getQuantity());
+        System.out.println();
+    }
+}
+
+class Vegetable extends Product {
+    public Vegetable(String name, double price, int quantity) {
+        super(name, price, quantity);
+    }
+
+    public void displayInfo() {
+        System.out.println("Vegetable: " + getName());
+        System.out.println("Price: " + getPrice());
+        System.out.println("Quantity: " + getQuantity());
+        System.out.println();
+    }
+}
+
+class Grocery extends Product {
+    public Grocery(String name, double price, int quantity) {
+        super(name, price, quantity);
+    }
+
+    public void displayInfo() {
+        System.out.println("Grocery: " + getName());
+        System.out.println("Price: " + getPrice());
+        System.out.println("Quantity: " + getQuantity());
+        System.out.println();
+    }
+}
