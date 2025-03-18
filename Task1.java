49e6844aeac0d05c672a937183950084f3680144
Diluxshana-2001 <diluxshana6@gmail.com>
2025-03-18 19:49:28 +0530

    Added a simple program to print a message

---
+++ Task1.java
@@ -0,0 +1,11 @@
+//Class name
+class Task1{
+    //Allowing the JVM to execute the code
+        public static void main(String[] args){
+        System.out.println("Hello World!!!");
+    }
+}
+
+// //
+// java Task1
+// Hello World!!!
