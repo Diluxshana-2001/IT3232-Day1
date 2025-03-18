2b82bbfab031aa9a194f5d4302ba76af686bab09
Diluxshana-2001 <diluxshana6@gmail.com>
2025-03-18 23:35:12 +0530

    created subclasses for printedbook, EBook, AudioBook

--- Task7.java
+++ Task7.java
@@ -6,9 +6,31 @@ abstract class Book {
         this.title=title;
         this.author=author;
     }
+    
     abstract void displayInfo();
+
+class PrintedBook extends Book {
+    public PrintedBook(String title, String author) {
+        super(title, author);
+    }
+}
+class EBook extends Book {
+    private String fileSize;
+    public EBook(String title, String author, String fileSize) {
+        super(title, author);
+        this.fileSize = fileSize;
+    }
 }
 
+class AudioBook extends Book {
+    private int pages; 
+
+    public AudioBook(String title, String author, int pages) {
+        super(title, author);
+        this.pages = pages;
+    }
+}
+}
 public class Task7 {
     
 }
