b2e97118f951667fdd92e68a156d1b5ff1fd7600
Diluxshana-2001 <diluxshana6@gmail.com>
2025-03-18 22:36:46 +0530

    created conditions for print the reverse order

--- Task5.java
+++ Task5.java
@@ -1,9 +1,22 @@
 import java.util.Scanner;
+
 public class Task5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
         int n = sc.nextInt();
 
-        
+        int mid =( n + 1 ) / 2;
+        for(int i = 1 ; i <= n ; i++){
+            if(i < mid ){
+                System.out.println((mid - 1 - i + 1)+ " ");
+            }
+            else if(i == mid){
+                System.out.println(mid);
+            }
+            else{
+                System.out.println((n - i + mid + 1)+ " ");
+            }
+        }
     }
 }
